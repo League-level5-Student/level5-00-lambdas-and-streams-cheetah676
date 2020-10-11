d9e099296b668183c9d252f56488b231b8eb83f8
cheetah676 <jake.mordan@gmail.com>
2020-10-11 11:25:34 -0700

    lambda buttons and methods done

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -33,6 +33,9 @@ public class LambdaButtons {
 			Random rand=new Random();
 			 System.out.println(rand.nextInt(1000000));
 		});
+		tellAJoke.addActionListener(e->{
+			JOptionPane.showMessageDialog(null, "Two hunters are out in the woods when one of them collapses. He doesn't seem to be breathing and his eyes are glazed. The other guy whips out his phone and calls the emergency services. \n He gasps, \"My friend is dead! What can I do?\" The operator says, \"Calm down. I can help. First, let's make sure he's dead.\" There is a silence; then a gun shot is heard. Back on the phone, the guy says, \"OK, now what?");
+		});
 		
 		window.setVisible(true);
 		window.pack();
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,7 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -13,13 +15,75 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String sCopy="";
+			for(int i=0; i<s.length(); i++) {
+				sCopy+=s.charAt(s.length()-i-1);
+				System.out.println(sCopy);
+			}
+		}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String sCopy="";
+			Random rand=new Random();
+			int charCase;
+			String chars="";
+			int capitals=0;
+			int lowercase=0;
+			for(int i=0; i<s.length(); i++) {
+				chars="";
+				charCase=rand.nextInt(2);
+				if(charCase==0) {
+					chars+=s.charAt(i);
+					chars=chars.toUpperCase();
+					sCopy+=chars;
+					capitals++;
+				}
+				else{
+					chars+=s.charAt(i);
+					chars=chars.toLowerCase();
+					sCopy+=chars;
+					lowercase++;
+				}
+				System.out.println(sCopy);
+			}
+			System.out.println(capitals);
+			System.out.println(lowercase);
+		}, "AP Human Geography");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String sCopy="";
+			for(int i=0; i<s.length(); i++) {
+				sCopy+=s.charAt(i);
+				sCopy+=".";
+				System.out.println(sCopy);
+			}
+		}, "Acronym");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			String sCopy="";
+			for(int i=0; i<s.length(); i++) {
+				if(s.charAt(i)=='a') {
+					System.out.println("deported");
+				}
+				else if(s.charAt(i)=='e') {
+					System.out.println("deported");
+				}
+				else if(s.charAt(i)=='i') {
+					System.out.println("deported");
+				}
+				else if(s.charAt(i)=='o') {
+					System.out.println("deported");
+				}
+				else if(s.charAt(i)=='u') {
+					System.out.println("deported");
+				}
+				else {
+					sCopy+=s.charAt(i);
+					System.out.println(sCopy);
+				}
+			}
+		}, "Vowels are illegal");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
